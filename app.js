16d01cb14eff9e128ee5495791ebebd561e85e63
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-06-22 20:13:24 +0600

    main nav a section animation done

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const nav = document.querySelector("nav")
 const header = document.querySelector("header")
+const mainNav = document.getElementById("main-nav")
 
 nav.addEventListener("click", ()=>{
     header.classList.toggle("black-nav")
     console.log("doing toggle")
     console.log(nav, header)
+    mainNav.classList.toggle("active")
 })
 
