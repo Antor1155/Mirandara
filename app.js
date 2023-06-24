05b80a6d69bc08f3ecaf0fccb608c401e208561a
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-06-24 14:21:09 +0600

    fade to chagne relative url done

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const nav = document.querySelector("nav")
 const header = document.querySelector("header")
 const mainNav = document.getElementById("main-nav")
+const main = document.querySelector("main")
 
 nav.addEventListener("click", ()=>{
     header.classList.toggle("black-nav")
     mainNav.classList.toggle("active")
 })
+
+// fade out when going to another link 
+function handleLink(targetLink){
+    document.querySelector("body").style.backgroundColor = "#C3BBB2"
+    main.style.opacity = 0;
+    nav.click()
+    setTimeout(()=>{
+        console.log("set out")
+        window.location.href = "/" + targetLink
+    }, 2000);
+}
